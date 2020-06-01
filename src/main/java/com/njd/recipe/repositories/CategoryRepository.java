9ece7a64623c8b26e58e53857a8ec4b8bc0212c0
nickd <nick.dixon@gmail.com>
2020-06-01 15:37:24 +0100

    Add repositories
    Repositories for model data

---
+++ src/main/java/com/njd/recipe/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.njd.recipe.repositories;
+
+import com.njd.recipe.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+
+}
---
+++ src/main/java/com/njd/recipe/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.njd.recipe.repositories;
+
+import com.njd.recipe.model.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
