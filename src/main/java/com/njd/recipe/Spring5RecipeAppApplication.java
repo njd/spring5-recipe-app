1f64649c09aede4123daf6c00c678feabb80f754
nickd <nick.dixon@gmail.com>
2020-06-17 10:52:13 +0100

    Fix Application package declaration
    Move Spring5RecipeAppApplication to com.njd.recipe

--- src/main/java/com/njd/recipe/Spring5RecipeAppApplication.java
+++ src/main/java/com/njd/recipe/Spring5RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package com.njd;
+package com.njd.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/njd/recipe/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/njd/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,7 +1,6 @@
 package com.njd.recipe.repositories;
 
 import com.njd.recipe.model.UnitOfMeasure;
-import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
